4b21b2ed45e7f519aecae2291a5623f1379b4a79
Kassim Ouattara <ouat.kassim@gmail.com>
2017-12-07 13:40:49 -0500

    Setting the template

---
+++ script.js
@@ -0,0 +1,11 @@
+// number
+// strings
+var person = {
+    firstName: 'Kassim',
+    lastName: 'Ouattara',
+    children: ['hamza', 'cruzita']
+}
+
+console.log('your first name is ' + person.firstName +' Your last name is ' + person.lastName + ' you have ' + person.children.join());
+
+
