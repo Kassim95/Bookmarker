7dd9ed5d73f0a3ccbbbb4ed92773703dfb538e08
Kassim Ouattara <ouat.kassim@gmail.com>
2017-12-07 14:08:08 -0500

    saving the form inputs in local storage

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-// number
-// strings
-var person = {
-    firstName: 'Kassim',
-    lastName: 'Ouattara',
-    children: ['hamza', 'cruzita']
+//Listen for form submit
+document.getElementById('myForm').addEventListener('submit', saveBookmark);
+
+function saveBookmark(e){
+    e.preventDefault();
+    
+    var siteName = document.getElementById('siteName');
+    var siteUrl = document.getElementById('siteUrl');
+    
+    var bookmark = {
+        name: siteName,
+        url: siteUrl 
+    }
+
+    if(localStorage.getItem('bookmarks') === null){
+        var bookmarks = [];
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    } else {
+        var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+        bookmarks.push(bookmark);
+        localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
 }
-
-console.log('your first name is ' + person.firstName +' Your last name is ' + person.lastName + ' you have ' + person.children.join());
-
-
